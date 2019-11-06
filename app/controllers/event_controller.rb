05352728b076bdf19a7fdb0921c91d07cd57282b
ombelinedej <ombeline.dj@gmail.com>
2019-11-06 02:25:43 +0100

    ast commit

---
+++ app/controllers/event_controller.rb
@@ -0,0 +1,31 @@
+class EventController < ApplicationController
+  before_action :authenticate_user, only: [:create]
+
+  def index
+  end
+
+  def new
+  end
+
+  def show 
+  end
+
+  def create
+
+    @event = Event.new(title: params["event_title"],
+                      description: params["event_description"],
+                      start_date: params["event_start_date"],
+                      duration: params["event_duration"],
+                      price: params["event_price"],
+                      location: params["event_location"],
+                      user_id: current_user.id)
+    
+
+  end
+end
+
+
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-
-root to: 'static_pages#index'  
-
+  resources :event
+  resources :user
+  resources :attendance, only: [:create, :destroy]
+  root 'static_pages#index'
+  devise_for :users
+  get 'event/index'
 
   get 'static_pages/index'
   get 'static_pages/secret'
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
